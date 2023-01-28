806507141d0be5620b066078beba3b799e76fde1
DyDlya <dacenkodaniil4@gmail.com>
2023-01-28 22:00:01 +0300

    vs code: organized the project

--- MobileApp/scr/main.py
+++ MobileApp/scr/main.py
@@ -1,15 +1,25 @@
 import kivy
+import yaml
+
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+
+from widgets import RootContainer
+
+
 
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return RootContainer()
 
+
+
+def main():
+    
+    MyApp().run()
 
 if __name__ == '__main__':
-    MyApp().run()
+    main()
---
+++ MobileApp/scr/widgets.py
@@ -0,0 +1,10 @@
+import kivy
+
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.lang import Builder
+
+Builder.load_file("MobileApp//kv-uix//RootContainer.kv")
+
+
+class RootContainer(AnchorLayout):
+    pass

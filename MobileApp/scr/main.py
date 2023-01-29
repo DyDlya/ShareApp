31e0ab85bed0729d06120e15cf42e20a8262eb44
DyDlya <dacenkodaniil4@gmail.com>
2023-01-29 12:30:10 +0300

    vscode: save project's paths

--- MobileApp/scr/main.py
+++ MobileApp/scr/main.py
@@ -1,10 +1,9 @@
 import kivy
 import yaml
-
+import os
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
-
 from widgets import RootContainer
 
 
@@ -15,10 +14,25 @@ class MyApp(App):
     def build(self):
         return RootContainer()
 
+def _SavePaths():
 
+    
+    project_dir:str = os.path.dirname(os.path.dirname(__file__))
+
+    # ProjectPath - полный путь проекта
+    # ScriptsPath - путь к папке со скриптами 
+    # UixTemplatesPath - путь к файлам разметки языка Kv
+    paths = {
+        "ProjectPath" : str(project_dir),
+        "ScriptsPath" : str(os.path.dirname(__file__)),
+        "UixTemplatesPath" : str(project_dir + "/kv-uix")
+    }
+
+    with open(paths["ScriptsPath"]+"/paths.yaml", 'w') as file:
+        yaml.dump(paths, file)
 
 def main():
-    
+    _SavePaths()
     MyApp().run()
 
 if __name__ == '__main__':
--- MobileApp/scr/widgets.py
+++ MobileApp/scr/widgets.py
@@ -1,10 +1,31 @@
 import kivy
+import yaml
 
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.lang import Builder
 
-Builder.load_file("MobileApp//kv-uix//RootContainer.kv")
+
+
+# Builder.load_file("MobileApp//kv-uix//RootContainer.kv")
+
+# class MainWidgets:
+
+#     projectDir:str
+#     scrDir:str
+#     uixDir:str
+    
+#     def _getPath():
+#         with open("paths.yaml", "r") as file:
+#             paths = file.read()
+#         projectDir = paths["ProjectPath"] 
+#         scrDir = paths["ScriptsPath"]
+#         uixDir = paths["UixTemplatesPath"]
+
+
+
 
 
 class RootContainer(AnchorLayout):
-    pass
+    pass    
+
+    

c4b99f785f4db90e89c5fe274ae0b3d97e9d9070
DyDlya <dacenkodaniil4@gmail.com>
2023-01-29 23:30:12 +0300

    pycharm: create base for LoginForm

--- MobileApp/scr/main.py
+++ MobileApp/scr/main.py
@@ -1,39 +1,28 @@
 import kivy
 import yaml
-import os
-kivy.require('2.1.0') # replace with your current kivy version !
+
+kivy.require('2.1.0')  # Версия Kivy
 
 from kivy.app import App
 from widgets import RootContainer
+from pathlib import *
 
+from kivy.config import Config
+
+Config.set("graphics", "width", "424")
+Config.set("graphics", "height", "860")
 
 
 
 class MyApp(App):
 
     def build(self):
-        return RootContainer()
+        return RootContainer(Path.cwd().parent)
 
-def _SavePaths():
-
-    
-    project_dir:str = os.path.dirname(os.path.dirname(__file__))
-
-    # ProjectPath - полный путь проекта
-    # ScriptsPath - путь к папке со скриптами 
-    # UixTemplatesPath - путь к файлам разметки языка Kv
-    paths = {
-        "ProjectPath" : str(project_dir),
-        "ScriptsPath" : str(os.path.dirname(__file__)),
-        "UixTemplatesPath" : str(project_dir + "/kv-uix")
-    }
-
-    with open(paths["ScriptsPath"]+"/paths.yaml", 'w') as file:
-        yaml.dump(paths, file)
 
 def main():
-    _SavePaths()
     MyApp().run()
+
 
 if __name__ == '__main__':
     main()
--- MobileApp/scr/widgets.py
+++ MobileApp/scr/widgets.py
@@ -1,31 +1,35 @@
 import kivy
 import yaml
-
+from pathlib import *
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.lang import Builder
 
 
+class WidgetsFound:
+    """
+    Класс-фундамент для возможности обращения любого виджета программы к project's paths
+    """
 
-# Builder.load_file("MobileApp//kv-uix//RootContainer.kv")
+    projectPath: Path
+    uixDir: str = "kv-uix"
+    uixPath: Path
 
-# class MainWidgets:
-
-#     projectDir:str
-#     scrDir:str
-#     uixDir:str
-    
-#     def _getPath():
-#         with open("paths.yaml", "r") as file:
-#             paths = file.read()
-#         projectDir = paths["ProjectPath"] 
-#         scrDir = paths["ScriptsPath"]
-#         uixDir = paths["UixTemplatesPath"]
+    def __init__(self, pPath: Path):  # (путь к проекту, путь к шаблонам)
+        self.projectPath = pPath
+        self.uixPath = pPath / self.uixDir
 
 
+class RootContainer(WidgetsFound, AnchorLayout):
+    def __init__(self, pPath: Path, **kwargs):
+        """
+        :param pPath: полный путь к проекту вне зависимости от системы
+        :param kwargs: необходимый параметр для конструктора класса AnchorLayout
 
-
-
-class RootContainer(AnchorLayout):
-    pass    
-
-    
+        """
+        WidgetsFound.__init__(self, pPath)  # Обращаемся к конструктору класса WidgetsFound
+        """
+        "Buider.load_file()" обязательно должен присутсвовать перед конструктором класса, для которого он
+        подгружает шаблон "classname.kv". Если это условие не будет выполнено, то дизайн не отобразиться 
+        """
+        Builder.load_file(str(self.uixPath / "RootContainer.kv"))  # Подгружается экран авторизации
+        AnchorLayout.__init__(self, **kwargs)
